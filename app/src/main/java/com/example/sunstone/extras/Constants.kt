9987de6b8b10478de54d6b34f3874dc49bf351da
Nishant Rishab <imreprisal@gmail.com>
2022-01-16 14:23:15 +0530

    data class added

---
+++ app/src/main/java/com/example/sunstone/extras/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.sunstone.extras
+
+object Constants {
+    const val BASE_URL = "https://api.pexels.com/v1/"
+    const val API_KEY = "563492ad6f91700001000001221396003fa24136b9faec0ebb1928d8"
+    const val START_PAGE = 1
+    const val PER_PAGE = 80
+}
---
+++ app/src/main/java/com/example/sunstone/interfaces/ItemClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.sunstone.interfaces
+
+interface ItemClickListener {
+    fun onItemClick(position: Int)
+}
